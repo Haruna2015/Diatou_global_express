8f74ff0350fee8e1327fd3bb3bf14eedd7b3e5e3
Haruna2015 <47338308+Haruna2015@users.noreply.github.com>
2026-06-05 13:11:34 +0300

    Create Firestore_service.dart

---
+++ Firestore_service.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db =
+      FirebaseFirestore.instance;
+
+  Future<void> addUserData(
+      String uid,
+      Map<String, dynamic> data) async {
+    await _db.collection('users').doc(uid).set(data);
+  }
+
+  Future<DocumentSnapshot> getUserData(
+      String uid) async {
+    return await _db
+        .collection('users')
+        .doc(uid)
+        .get();
+  }
+
+  Future<void> updateUserData(
+      String uid,
+      Map<String, dynamic> data) async {
+    await _db
+        .collection('users')
+        .doc(uid)
+        .update(data);
+  }
+}
